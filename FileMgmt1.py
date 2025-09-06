2719dd399c75f49f5c2ba2923e8e3ec0b99fa20b
Vish2174 <sydvishaus@gmail.com>
2025-09-06 16:08:20 +0100

    Added ability to read log file

--- FileMgmt1.py
+++ FileMgmt1.py
@@ -12,7 +12,8 @@ class SmartOrganizer:
         self.categories = {
             'Documents':['pdf','docx','doc','txt'],
             'Data':['xlsx','xls','csv','json'],
-            'Media':['pptx']
+            'Media':['pptx','ppt','jpeg','mpg'],
+            'Applications':['dmg','zip','exe']
         }
 
     def fileOrganize(self):
@@ -32,7 +33,8 @@ class SmartOrganizer:
             "directories":{
                 "folder1":foldersCreated[0],
                 "folder2":foldersCreated[1],
-                "folder3":foldersCreated[2]
+                "folder3":foldersCreated[2],
+                "folder4":foldersCreated[3]
             }
         }
 
@@ -51,31 +53,46 @@ class SmartOrganizer:
                 dest_path = os.path.join(foldersCreated[1],files)
                 shutil.move(src_path,dest_path)
 
-            elif files.endswith('pptx') or files.endswith('ppt'):
+            elif files.endswith('pptx') or files.endswith('ppt') or files.endswith('jpeg') or files.endswith('mpg'):
                 src_path = os.path.join(pathName,files)
                 dest_path = os.path.join(foldersCreated[2],files)
+                shutil.move(src_path,dest_path)
+
+            elif files.endswith('dmg') or files.endswith('zip') or files.endswith('exe'):
+                src_path = os.path.join(pathName,files)
+                dest_path = os.path.join(foldersCreated[3],files)
                 shutil.move(src_path,dest_path)
 
 
         # Write Data into log     
 
         with open(self.log_file,'a') as file:
-            file.write(json.dumps(op_data) + '\n')
-        
-    
+            file.write(json.dumps(op_data_member) + '\n')
+
+
+        with open(self.log_file,'r') as file:
+            for line in file:
+                record = json.loads(line.strip())
+                for key,folder in record['directories'].items():
+                    print (folder)
+                
+
+
 # Define Main Function
 
 def main():
     # Get user input
 
+
+
     pathName = str(sys.argv[1])
     smartOrg1 = SmartOrganizer(pathName)
     smartOrg1.fileOrganize()
-    
-    
-
+     
 # Calling Main
 
 if __name__ == '__main__':
     main()
+
+
     
